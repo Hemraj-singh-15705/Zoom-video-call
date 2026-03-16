2304293c8769f2bede1ed48e5a7eb62393be5b99
Hemraj-singh-15705 <hemraj15705@gmial.com>
2026-03-16 19:22:26 +0530

    env file

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,10 @@
-const IS_PROD = window.location.hostname !== "localhost";
+const IS_PROD = window.location.hostname !== "localhost" && window.location.hostname !== "127.0.0.1";
 
 const server = IS_PROD ?
     "https://linkupbackend-p4kd.onrender.com" :
     "http://localhost:8000";
+
+console.log("Current Server URL:", server);
+console.log("Current Hostname:", window.location.hostname);
 
 export default server;
