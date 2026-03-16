7227ecc57e821a77bbeda906014b2f2033f83599
Hemraj-singh-15705 <hemraj15705@gmial.com>
2026-03-16 19:24:49 +0530

    cons

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,7 +22,10 @@ const io = connectToSocket(server);
 
 
 app.set("port", (process.env.PORT || 8000))
-app.use(cors());
+app.use(cors({
+    origin: ["https://linkup-yy69.onrender.com", "http://localhost:3000"],
+    credentials: true
+}));
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
